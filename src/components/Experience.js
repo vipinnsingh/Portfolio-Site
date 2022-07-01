818c2f510ebc169c84c93ff644b7aa79690ca3bc
vipinnsingh <107580455+vipinnsingh@users.noreply.github.com>
2022-07-02 00:42:14 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,14 +11,14 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <i>Code Eater</i> - <strong>Solidity Development</strong>, 2018-2019
+                    <i>Code Eater</i> - <strong>Solidity Development</strong>, 2022
                     <ul>
                         <li>Studied Smart Contracts development</li>
                         <li>Deploying contracts on local Blockchain</li>
                     </ul>
                 </li>
                 <li>
-                    <i>Dapp University</i> - <strong>Blockchain Development</strong>, 2019-2022
+                    <i>Dapp University</i> - <strong>Blockchain Development</strong>, 2022
                     <ul>
                         <li>Learnt about Truffle, Ganache, Web3.js and other tools</li>
                         <li>Studied the concept of Defi &amp; other Web3 Products</li>
