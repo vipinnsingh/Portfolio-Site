3c1f79d60f06d4c9a99a60f78c9fdaf155903cf8
vipinnsingh <107580455+vipinnsingh@users.noreply.github.com>
2022-07-02 00:39:11 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,17 +11,17 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <i>TechFlow</i> - <strong>Blockchain Developer Intern</strong>, 2018-2019
+                    <i>Code Eater</i> - <strong>Solidity Development</strong>, 2018-2019
                     <ul>
-                        <li>Studied DApp development</li>
-                        <li>Assisted Research &amp; Development on Web3 Products</li>
+                        <li>Studied Smart Contracts development</li>
+                        <li>Deploying contracts on local Blockchain</li>
                     </ul>
                 </li>
                 <li>
-                    <i>SpiderChain</i> - <strong>Blockchain Developer</strong>, 2019-2022
+                    <i>Dapp University</i> - <strong>Blockchain Development</strong>, 2019-2022
                     <ul>
-                        <li>Contributed to smart contract development on client's platform</li>
-                        <li>Peer reviewed &amp; Audited Solidity code</li>
+                        <li>Learnt about Truffle, Ganache, Web3.js and other tools</li>
+                        <li>Studied the concept of Defi &amp; other Web3 Products</li>
                     </ul>
                 </li>
             </ul>
