22481a1d476caa595371b37342e6de1e51b5bff8
vipinnsingh <vipinsingh262000@gmail.com>
2022-12-13 00:18:42 +0530

    css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,27 +11,27 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>ERC20 token</h3>
+                    <h3>DAPPS</h3>
                     <img src={uniswap} alt="Uniswap Swap Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Facilis consectetur facere neque assumenda quasi aliquid,
                         perferendis ratione rerum dolorem.
                     </p>
 
-                    <a href="https://ethereum.org/en/developers/docs/standards/tokens/erc-20/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vipinnsingh/Solidity/blob/main/ERC20token.sol" target="_blank" className="button">Code</a>
+                    <a href="http://165.22.213.170/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
-                    <h3>Covid19 Certificate</h3>
+                    <h3>UniFarm</h3>
                     <img src={compound} alt="Compound Landing Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Facilis consectetur facere neque assumenda quasi aliquid,
                         perferendis ratione rerum dolorem.
                     </p>
 
-                    <a href="https://covid19.gov.lv/en/covid-19/about-covid-19/digital-covid-19-certificate" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vipinnsingh/Solidity/blob/main/covid19coldchain.sol" target="_blank" className="button">Code</a>
+                    <a href="https://app.unifarm.co/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vipinsinghh" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
